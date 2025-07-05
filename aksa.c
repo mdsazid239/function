8fa2906e93d5b9457f92f7f3db1e169558e06691
Md Sazid <141584613+mdsazid239@users.noreply.github.com>
2025-07-05 12:19:08 +0530

    Add files via upload
    this is c code

---
+++ aksa.c
@@ -0,0 +1,15 @@
+//  Display this GP - 3,12,48,.. upto ‘n’ terms.
+// #include<stdio.h>
+// #include<math.h>
+// int main ()
+// {
+//     int a=3;
+//     int r=4;
+//     int term=a;
+//     for(int i =1;i<=n;i++)
+//     {
+//         printf("%d",i);
+//         term=a*r
+//     }
+//     return 0;
+// }
